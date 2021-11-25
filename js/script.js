b56f005628cec6ba8649117ff4b17889e2902d2c
Ben <willn_88@hotmail.com>
2021-11-25 14:17:56 +0000

    First branch commit, before function separation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $("document").ready(function() {
 
-//draw map onto <div> element.
+//draw map onto <div id="map"> element.
 /////////////////////////////////////////////////////////////////////////////////////////////
-	var myMap = L.map('map',{
+	var myMap = L.map('map',
+		{
+			zoomSnap: 1,
+			zoomDelta: 1,
+			// animate: true,
+			// duration: 5
 		//worldCopyJump: true
 	});
 
@@ -16,12 +21,12 @@ $("document").ready(function() {
 		ext: 'png'}
 	).addTo(myMap);
 
-	
+//Determine current country, highlight borders and add country names to datalist.
+/////////////////////////////////////////////////////////////////////////////////////////////	
 	$.ajax({
 		url: "php/getgeoJSON.php", 
 		type: 'GET',
 		dataType: 'json',
-		data: {},
 		success: function(geoJsonOutput) {	//get geo.json object from php data provider.
 			function successCb(position) {
 				
@@ -36,23 +41,39 @@ $("document").ready(function() {
 						lat: currentLat,
 						lng: currentLng
 					},
-					success: function(countryInfoOutput) {	//get current country name.
+					success: function(countryInfoOutput) {	//countryInfoOutput is the array returned from the php request.
+						
 						let currentCountryName = countryInfoOutput['data']['countryName'];
 						let geoJsonData = geoJsonOutput['geoJsonData'];
 						
-						let geoJsonLayer = L.geoJSON(geoJsonData, {onEachFeature: function(feature, layer) {
+						let allCountryLayer = L.geoJSON(geoJsonData);								//create geoJSON object.
+						allCountryLayer.setStyle({color: 'black', weight: 2, fill: true, fillOpacity: 0.01});	
+						allCountryLayer.addTo(myMap);
 
-							if (currentCountryName == feature['properties']['name']) {
-								let currentCountryLayer = layer;
-								currentCountryLayer.addTo(myMap);
+						allCountryLayer.eachLayer(function (layer) {
+							if (layer['feature']['properties']['name'] == currentCountryName) {
+								let homeCountry = layer;
+								homeCountry.setStyle({color: 'purple', fill: true, fillOpacity: 0.25});
 							}
+						});
 
-						}});
+						myMap.fitBounds([
+							[57.712, -7],
+							[49.774, -1]
+						], );
+
+						allCountryLayer.on('click', function click(e) {
+							allCountryLayer.setStyle({color: 'black', weight: 2, fill: true, fillOpacity: 0.01});
+							let clickedCountryId = e['layer']['_leaflet_id'];
+							let countryLayer = allCountryLayer.getLayer(clickedCountryId);
+							countryLayer.setStyle({color: 'red', fill: true, fillOpacity: 0.25});
+						});
 					},
 					error: function(jqXHR, textStatus, errorThrown) {
 						// your error code
-					}	
+					}
 				});
+
 			};
 		
 			function errorCb(errorMsg) {
@@ -60,15 +81,37 @@ $("document").ready(function() {
 			};
 		
 			navigator.geolocation.getCurrentPosition(successCb, errorCb)
-
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
 			// your error code
 		}
+		
 	});
 	
-//draw map onto <div> element.
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-});
 	
+
+//Add border of selected country from datalist.
+/////////////////////////////////////////////////////////////////////////////////////////////	
+	$('#countriesInput').on('change', function() {
+		//remove layers
+		$.ajax({
+			url: "php/getgeoJSON.php", 
+			type: 'GET',
+			dataType: 'json',
+			success: function(geoJsonOutput) {	//get geo.json object from php data provider.
+				let geoJsonData = geoJsonOutput['geoJsonData'];
+
+				let selectedCountry = L.geoJSON(geoJsonData, {onEachFeature: function(feature, layer) {
+					if ($('#countriesInput').val() == feature['properties']['name']) {
+						return layer;
+					}
+				}});
+				selectedCountry.addTo(myMap);
+				// myMap.fitBounds(layer.getBounds());
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		});
+	});
+});
